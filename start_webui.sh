806af747ad2caa20ea405bb8f1efa75406496b27
AnyaCoder <a2983352531@outlook.com>
2024-02-21 21:56:11 +0800

    Linux start fixed.

--- start_webui.sh
+++ start_webui.sh
@@ -3,17 +3,15 @@
 # Setting up the environment variables
 export no_proxy="localhost,127.0.0.1,0.0.0.0"
 export PYTHONPATH=$(dirname "$0")
-export PYTHON_FOLDERPATH="~/miniconda/envs/vits/"
-export SCRIPT_FOLDERPATH="~/miniconda/envs/vits/Scripts/"
 export CARGO="~/.cargo/bin/"
 export RUST_LOG=info
-export PATH="$CARGO:$PYTHON_FOLDERPATH:$SCRIPT_FOLDERPATH:$PATH"
+export PATH="$CARGO:$PATH"
 
 # Echo the PATH to ensure it's set correctly
 echo $PATH
 
 # Run the Python script
-${PYTHON_FOLDERPATH}python fish_speech/webui/manage.py
+python fish_speech/webui/manage.py
 
 # Wait for a key press before exiting
 read -p "Press any key to continue..." -n1 -s
