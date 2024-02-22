35a837abb99bf4c995dec733e54cf6fb04fad223
AnyaCoder <a2983352531@outlook.com>
2024-02-22 14:21:29 +0800

    Runtime

--- start_webui.bat
+++ start_webui.bat
@@ -5,11 +5,10 @@ chcp 65001
 set no_proxy="localhost, 127.0.0.1, 0.0.0.0"
 set PYTHONPATH=%~dp0
 set GIT_HOME=%~dp0PortableGit\bin
-set PYTHON_FOLDERPATH=D:\miniconda\envs\vits\
-set SCRIPT_FOLDERPATH=D:\miniconda\envs\vits\Scripts\
-set CARGO=%USERPROFILE%\.cargo\bin\
+set PYTHON_FOLDERPATH=%~dp0vits\
+set SCRIPT_FOLDERPATH=%~dp0vits\Scripts\
 
-set NEW_PATH="%CARGO%;%PYTHON_FOLDERPATH%;%SCRIPT_FOLDERPATH%;%PATH%;"
+set NEW_PATH="%PYTHON_FOLDERPATH%;%SCRIPT_FOLDERPATH%;%PATH%;"
 set PATH=%NEW_PATH%
 
 echo %PATH%
